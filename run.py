34faf7d376de1a8dc74e387f957e2f7a991c10ec
Mathijs Riezebos <robijn.bird@gmail.com>
2017-11-29 04:35:24 +0100

    Generalisation and configurabilty
    - Expanded default config with tooltips
    - Added extra config options
    - Changed mentioning of "snip"  into "path", since snip is not the only
    program capable of creating text files.
    - Added option to use a standalone bot instead of a self-bot **and**
    made it default, because self-bots are against Discord's TOS.
    - The text file will not even get read if its last modified date has not
    changed.

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import inspect
 import sys
 import time
 import os
-
+import platform
 import discord
 from discord.ext import commands
 from logbook import Logger, StreamHandler, FileHandler
@@ -15,16 +15,44 @@ logger.handlers.append(FileHandler("last-run.log", bubble=True, mode="w"))
 
 logger.debug("Loading config files")
 
-default_config = "[Config]\ntoken = \nsnip = "
+default_config = "[Config]\n;Command prefix used by the 'Music Bee Now Playing' bot\n;def: dms.\ncommandPrefix = dms.\n\n;Whether the targeted user is a bot or a client (also, called self-bot)\n;The latter is technically against Discord's TOS. USE AT YOUR OWN RISK\n;def: true\nbot = true\n\n;Token of the client or bot you want to change the 'playing' status of\n;def: \ntoken = \n\n;Path to the text file you want to load your status message from\n;def: \npath = \n\n;Minimum length of the contents of the message file. If less, the file will be considered empty, as if no song is playing.\n;def: 10\nminLength = 10"
 
 config = configparser.ConfigParser()
 
+#config entries
+commandPrefix = ""
+isBot = True
 token = ""
-snip = ""
+path = ""
+minLength = 0
 
+#loading config
 if os.path.exists("config.ini"):
     config.read("config.ini")
 
+    #commandPrefix
+    try:
+        commandPrefix = config['Config']['commandPrefix']
+    except KeyError:
+        logger.critical("No commandPrefix found in config, please ensure that the config formatting is correct")
+        time.sleep(5)
+        exit(1)
+
+    if commandPrefix == "":
+        logger.critical("No commandPrefix set! Exiting")
+        time.sleep(5)
+        exit(1)
+        exit(1)
+
+    #isBot
+    try:
+        isBot = config.getboolean('Config', 'bot')
+    except KeyError:
+        logger.critical("No bot found in config, please ensure that the config formatting is correct")
+        time.sleep(5)
+        exit(1)
+
+    #token
     try:
         token = config['Config']['token']
     except KeyError:
@@ -37,17 +65,26 @@ if os.path.exists("config.ini"):
         time.sleep(5)
         exit(1)
 
+    #path
     try:
-        snip = config['Config']['snip']
+        path = config['Config']['path']
     except KeyError:
-        logger.critical("No path to snip found in config, please ensure that the config formatting is correct")
+        logger.critical("No path to file found in config, please ensure that the config formatting is correct")
         time.sleep(5)
         exit(1)
 
-    if snip == "":
-        logger.critical("No path to snip set! Exiting")
+    if path == "":
+        logger.critical("No path set! Exiting")
+        time.sleep(5)
+
+    #minLength
+    try:
+        minLength = config.getint('Config', 'minLength')
+    except KeyError:
+        logger.critical("No minimum file-length found in config, please ensure that the config formatting is correct")
         time.sleep(5)
         exit(1)
+        minLength
 
 else:
     logger.error("No config file, creating one now")
@@ -59,7 +96,7 @@ else:
 
 logger.info("Config loaded")
 
-bot = commands.Bot(command_prefix=['m.'], self_bot=True)
+bot = commands.Bot(command_prefix=[commandPrefix], self_bot= not isBot)
 bot.remove_command('help')
 
 
@@ -77,36 +114,51 @@ async def _quit():
     exit(0)
 
 
+def modification_date(path_to_file):
+    if platform.system() == 'Windows':
+        return os.path.getmtime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_mtime
+        except AttributeError:
+            return stat.st_birthtime
+
+
 async def music_loop():
     await bot.wait_until_ready()
     await asyncio.sleep(1)
     last_song = ""
+    timeLastModified = 0
     while not bot.is_closed:
-        song = pull_song()
-        if song != last_song:
-            last_song = song
-            if song == "":
-                await bot.change_presence(afk=True, status=discord.Status.invisible, game=None)
-                logger.info("Cleared Discord Status because no song is playing")
-            else:
-                await bot.change_presence(
-                    afk=True,
-                    status=discord.Status.invisible,
-                    game=discord.Game(name=song, type=2)
-                    )
-                logger.info(f"Set Discord status to {song.encode('ascii', 'ignore').decode()}")
+        newTimeLastModified = modification_date(path)
+        if timeLastModified != newTimeLastModified:
+            timeLastModified = newTimeLastModified
+            song = pull_song(path)
+            if song != last_song:
+                last_song = song
+                if song == "" or len( song ) < minLength:
+                    await bot.change_presence(afk=True, status=discord.Status.invisible, game=None)
+                    logger.info("Cleared Discord Status because no song is playing")
+                else:
+                    await bot.change_presence(
+                        afk=True,
+                        status=discord.Status.idle,
+                        game=discord.Game(name=song, type=2)
+                        )
+                    logger.info(f"Set Discord status to: Listening to {song.encode('ascii', 'ignore').decode()}")
         await asyncio.sleep(8)
 
 
-def pull_song():
-    with open(snip, encoding="utf-8") as f:
+def pull_song(path_to_file):
+    with open(path_to_file, encoding="utf-8") as f:
         return f.read()
 
 
 try:
     logger.info("Logging in")
     bot.loop.create_task(music_loop())
-    bot.run(token, bot=False)
+    bot.run(token, bot=isBot)
 except discord.errors.LoginFailure:
     logger.critical("Log in failed, check token!")
     time.sleep(5)
